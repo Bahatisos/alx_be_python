24b09d9619f4b3e57d6fccb1fae875fd4ec590eb
Bahatisos <lumumbasospeter1@gmail.com>
2025-10-12 19:38:22 +0300

    polymrphisim

---
+++ oop/polymorphism_demo.py
@@ -0,0 +1,32 @@
+
+
+import math
+
+# Base class
+
+
+class Shape:
+    def area(self):
+
+        raise NotImplementedError("Subclasses must implement the area method.")
+
+
+# Derived class: Rectangle
+class Rectangle(Shape):
+    def __init__(self, length, width):
+        self.length = length
+        self.width = width
+
+    # Override area method
+    def area(self):
+        return self.length * self.width
+
+
+# Derived class: Circle
+class Circle(Shape):
+    def __init__(self, radius):
+        self.radius = radius
+
+    # Override area method
+    def area(self):
+        return math.pi * (self.radius ** 2)
