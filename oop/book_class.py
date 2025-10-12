797c3ccc461e25c5fb0905fdd9f738cf52a72583
Bahatisos <lumumbasospeter1@gmail.com>
2025-10-12 19:32:25 +0300

    book class

---
+++ oop/book_class.py
@@ -0,0 +1,20 @@
+# book_class.py
+
+class Book:
+    # Constructor: Initializes the Book object
+    def __init__(self, title, author, year):
+        self.title = title
+        self.author = author
+        self.year = year
+
+    # String representation: Used by print() or str()
+    def __str__(self):
+        return f"{self.title} by {self.author}, published in {self.year}"
+
+    # Official representation: Used for debugging or recreating the object
+    def __repr__(self):
+        return f"Book('{self.title}', '{self.author}', {self.year})"
+
+    # Destructor: Called when the object is deleted
+    def __del__(self):
+        print(f"Deleting {self.title}")
